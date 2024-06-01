809fff17b17c8eb9e55378a2f562c47eab10ddfb
thgnommy <th.brandoli@gmail.com>
2024-06-01 21:54:20 +0200

    updated game mode

--- Source/ToonyPlatformer/Private/Controllers/ToonyPlatformerGameMode.cpp
+++ Source/ToonyPlatformer/Private/Controllers/ToonyPlatformerGameMode.cpp
@@ -1,7 +1,6 @@
 // Copyright Epic Games, Inc. All Rights Reserved.
 
 #include "Controllers/ToonyPlatformerGameMode.h"
-#include "Characters/ToonyPlatformerCharacter.h"
 #include "UObject/ConstructorHelpers.h"
 
 AToonyPlatformerGameMode::AToonyPlatformerGameMode()
